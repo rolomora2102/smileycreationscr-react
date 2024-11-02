201ddff3dff930d5e6ec7cd47a11a0d3ca7ea029
rmorag2102 <rmorag@ucenfotec.ac.cr>
2024-11-01 22:40:10 -0600

    setting up url

--- src/login/src/services/api.js
+++ src/login/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://smileycreationscr.com/api' : 'http://localhost:3001/api',
+  //baseURL: process.env.NODE_ENV === 'production' ? 'https://smileycreationscr.com/api' : 'http://localhost:3001/api',
+  baseURL: 'https://smileycreationscr.com/api'
 });
 
 api.interceptors.request.use((config) => {
